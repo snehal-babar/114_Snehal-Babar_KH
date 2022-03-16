19b0eb5152ff3cc6f75adce1ee402a39499344de
snehal-babar <101325792+snehal-babar@users.noreply.github.com>
2022-03-17 01:32:38 +0530

    Add files via upload

---
+++ Assignment3.2/Hfp.java
@@ -0,0 +1,22 @@
+class Hfp
+{
+public static void main( String args[])
+{
+	
+   for(int i=1;i<=6;i++)
+    {
+		for(int j=i;j<6;j++)
+	    {  
+		   System.out.print(" ");
+		}
+	    for(int k=1;k<=i;k++)
+	    {  
+		   if(i==6||i==k||k==1)
+		   System.out.print("* ");
+		   else
+		   System.out.print("  ");
+		}
+		System.out.println();
+    }
+}
+}
